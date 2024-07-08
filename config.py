afd215161a8e0c099a0d61aeb5af8d03d8fc589f
Mrlabani <158763270+Mrlabani@users.noreply.github.com>
2024-07-09 00:35:32 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "23990433")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6237026610:AAHhvQbM1nH4bDCAw80Dmx9rCmeGBbMdmQg") # ⚠️ Required
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "autofilter2_bot") # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "22419004")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "34982b52c4a83c2af3ce8f4fe12fe4e1") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7492087293:AAE6MNgRc9Q-hudikdYPhsvv7a4R1YDjBEY") # ⚠️ Required
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "YTDOWNLOAD5_ROBOT") # ⚠️ Required
    
     # database config
     DB_URL  = os.environ.get("DB_URL","mongodb+srv://sankar:sankar@sankar.lldcdsx.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
@@ -16,17 +16,17 @@ class Config(object):
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     TG_MAX_SIZE = 2040108421
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5821871362').split()] # ⚠️ Required
+    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/3999ba9e4ce962ff15a78.jpg")
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6742022802').split()] # ⚠️ Required
     FORCE_SUB_TEXT = os.environ.get('FORCE_SUB_TEXT', "**Yᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ ᴏᴜʀ Bᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ sᴏ ʏᴏᴜ ᴅᴏɴ'ᴛ ɢᴇᴛ ᴛʜᴇ ғɪʟᴇ...\n\nIғ ʏᴏᴜ ᴡᴀɴᴛ ᴛʜᴇ ғɪʟᴇ, ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ '❆ Jᴏɪɴ Oᴜʀ Bᴀᴄᴋ-Uᴘ Cʜᴀɴɴᴇʟ ❆' ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴀɴᴅ ᴊᴏɪɴ ᴏᴜʀ ʙᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ, ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ '↻ Tʀʏ Aɢᴀɪɴ' ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ...\n\nTʜᴇɴ ʏᴏᴜ ᴡɪʟʟ ɢᴇᴛ ᴛʜᴇ ғɪʟᴇs...**")
-    FORCE_SUB = os.environ.get('FORCE_SUB', 'SK_MoviesOffl') # ⚠️ Required
-    AUTH_CHANNEL = -1001811608554  # Updated AUTH_CHANNEL value
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001870015374")) # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', 'NOOBPrivate') # ⚠️ Required
+    AUTH_CHANNEL = -1002246016095  # Updated AUTH_CHANNEL value
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002204853756")) # ⚠️ Required
 
     
     # Mega User Account ⚠️ Only Set When you have Pro or Enterprise Mega Account
-    MEGA_EMAIL = os.environ.get("ak14147800@gmail.com")
-    MEGA_PASSWORD = os.environ.get("thakur#12")
+    MEGA_EMAIL = os.environ.get("labani99@outlook.com")
+    MEGA_PASSWORD = os.environ.get("@Labani25")
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
@@ -54,13 +54,7 @@ Click The Buttons Below To Search
 
     ABOUT_TXT = """<b>╭───────────⍟
 ├🤖 ᴍy ɴᴀᴍᴇ : {}
-├👨‍💻 Pʀᴏɢʀᴀᴍᴇʀ : <a href=https://t.me/Snowball_Official>𝓢𝓝𝓞𝓦𝓑𝓐𝓛𝓛</a>
-├👑 Instagram : <a href=https://www.instagram.com/ritesh6_>C-Insta</a> 
-├☃️ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/+HzGpLAZXTxoyYTNl>𝖱𝖮𝖮𝖥𝖨𝖵𝖤𝖱𝖲𝖤</a>
-├📕 Lɪʙʀᴀʀy : <a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>
-├✏️ Lᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>Pyᴛʜᴏɴ 3</a>
-├💾 Dᴀᴛᴀ Bᴀꜱᴇ: <a href=https://cloud.mongodb.com>Mᴏɴɢᴏ DB</a>
-├🌀 ᴍʏ ꜱᴇʀᴠᴇʀ : <a href=https://dashboard.heroku.com>Heroku</a>
+├👨‍💻 Pʀᴏɢʀᴀᴍᴇʀ : <a href=https://t.me/l_abani>ᑎOOᗷ</a>
 ╰───────────────⍟ """
 
     HELP_TXT = """
@@ -70,7 +64,7 @@ Tʜɪs Bᴏᴛ Wɪʟʟ Hᴇʟᴘ Yᴏᴜ Tᴏ Dᴏᴡɴʟᴏᴀᴅ Fᴏʟʟᴏ�
 ⊚ Mega
 ⊚ PornHub
 
-❗ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/Snowball_official>𝑺𝑼𝑷𝑷𝑶𝑹𝑻</a>
+❗ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/NOOBPrivate>𝐒𝐔𝐏𝐏𝐎𝐑𝐓</a>
 """
 
 
